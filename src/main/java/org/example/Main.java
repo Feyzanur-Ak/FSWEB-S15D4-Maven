76154165b0483397140c3f19e91b2cdfca1cf219
Feyzanur-Ak <senturkfeyzanur96@gmail.com>
2025-01-23 14:20:15 +0300

    Veri Yapıları Challenge yapıldı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(checkForPalindrome("hello"));
+    }
+
+    public static boolean checkForPalindrome(String input){
+      String cleanedInput="";
+      for(int i=0;i<input.length();i++){
+          char c=input.charAt(i);
+          if(Character.isLetterOrDigit(c)){
+              cleanedInput += Character.toLowerCase(c);
+          }
+  }
+      String reversedInput="";
+      for(int i=cleanedInput.length()-1;i>=0;i--){
+          reversedInput += cleanedInput.charAt(i);
+      }
+
+      return cleanedInput.equals(reversedInput);
     }
 
 }
